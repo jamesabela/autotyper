ad98a8de2286e85d43e08d28e9f30bde05219b9d
James Abela <abela.james@gmail.com>
2022-02-12 12:05:32 +0800

    Added a few key comments

--- main.py
+++ main.py
@@ -1,45 +1,51 @@
 """
-Can't handle # sometimes
+watch out for blank lines
+watch out for using the word space at end of line
+Please be careful which window this types into
 """
 
 import pyautogui
 import time
-spaces = 0
-delay_speed = 0.1
 
-def tabbing_mech(line):
+spaces = 0 
+delay_speed = 0.1 #Bigger number slower typing
+
+
+def tabbing_mech(line): #Needed for IDEs
     global spaces
     count_space = 0
     for char in line:
-        if char ==" ":
-            count_space +=1
-        else: break
-    print(count_space,spaces,line)
+        if char == " ":
+            count_space += 1
+        else:
+            break
+    print(count_space, spaces, line)
 
     if spaces > count_space:
-        back_tab = (spaces - count_space) // 4
+        back_tab = (spaces - count_space) // 4 #Number of spaces per indent
         spaces = count_space
         print("tab back")
         for i in range(back_tab):
             pyautogui.keyDown('shift')
             pyautogui.press('tab')
             pyautogui.keyUp('shift')
-        return line.replace("  ","")
+        return line.replace("  ", "")
     elif count_space == 0:
         return line
     elif spaces == count_space:
-        return line.replace("  ","")
+        return line.replace("  ", "")
     elif spaces < count_space:
         print("indenting")
         spaces = count_space
-        return line.replace("  ","")
+        return line.replace("  ", "")
 
 
-for i in range(3):
+for i in range(3): #Seconds before it starts typing
     time.sleep(1)
     print(i)
 
-with open("code", 'r') as f:
+with open("code.txt", 'r') as f:
     for lines in f:
-            tabbed_lines = tabbing_mech(lines)
-            pyautogui.typewrite(tabbed_lines,delay_speed)
+        #type_me = lines #Use this if on a word processor
+        type_me = tabbing_mech(lines) #Used for IDEs
+        pyautogui.typewrite(type_me, delay_speed)
